e10a062dfb00d4c36b6058eb4140e7f57c2cb754
Bruno Marques <bruno@brunomarques.com>
2025-06-18 15:36:20 -0300

    Atualiza versao

--- proxmox_qdevice/run.sh
+++ proxmox_qdevice/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x
+
 echo "Iniciando corosync-qnetd escutando em: ${listen_address:-0.0.0.0}"
 
-/usr/bin/corosync-qnetd -l "${listen_address:-0.0.0.0}"
-echo "corosync-qnetd finalizou"
-tail -f /dev/null
+exec /usr/bin/corosync-qnetd -l "${listen_address:-0.0.0.0}" -f -d
